431e7b0943bc9a056ce818aea22a6f5795fba68b
elchupacabras <josuemrtj@gmail.com>
2016-04-10 22:30:15 +0200

    empezando en serio con web de porno

---
+++ cripto-videos.html
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<!--[if IE 7 ]><html class="ie7" lang="es"> <![endif]-->
+<!--[if IE 8 ]><html class="ie8" lang="es"> <![endif]-->
+<!--[if IE 9 ]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml" lang="es"> <![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!-->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="es">
+<!--<![endif]-->
+<head>
+    <title>Videos | Los Criptozoos</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Seo Meta -->
+    <meta name="description" content="Videos | Band - Music & Band Site Template HTML5 and CSS3">
+    <meta name="keywords" content="Video, remix, music, light, dark, multi purpose, band, css3, html5">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/style.css" id="dark" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/icons/icons.css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=EmulateIE8; IE=EDGE" />
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/icons/font-awesome-ie7.min.css" />
+    <![endif]-->
+</head>
+
+
+<!-- popup login -->
+
+<!-- popup login -->
+
+
+       <!-- end social -->
+          <!-- end form -->
+          <!-- search -->
+       <!-- little head -->
+       <!-- row -->
+
+
+
+
+            <?php include "sections/header.php"; ?>
+
+
+            <!-- end nav -->
+
+      <!-- row -->
+   <!-- headdown -->
+<!-- end header -->
+
+<div class="under_header">
+        <img src="images/assets/breadcrumbs14.png" alt="#">
+</div><!-- under header -->
+
+<div class="page-content back_to_up">
+    <!-- breadcrumb -->
+    </div><!-- row -->
+
+    <div class="row row-fluid clearfix mbf">
+        <div class="posts">
+            <div class="def-block">
+                <ul class="tabs">
+                    <li><a href="#Latest" class="active"> Videos youtube </a></li>
+                    <li><a href="#Featured"> Video montajes </a></li>
+                    <li><a href="#Lightbox"> The BCFM Sunday Rock Sow </a></li>
+                </ul><!-- tabs -->
+
+                <ul class="tabs-content">
+                    <li id="Latest" class="active">
+                        <div class="video-grid">
+
+                            <a href="criptoplayvideo.php?id=sxxtLutPdEE" class="grid_3">
+                                <img src="images/assets/cadaver2.png" alt="#">
+                                <span><strong>Los Criptozoos</strong>La danza del cadaver</span>
+
+                            </a>
+                            <a href="criptoplayvideo.php?id=yauvpc91tM0" class="grid_3">
+                                <img src="images/assets/trella.png" alt="#">
+                                <span><strong>Los Criptozoos</strong>Como una estrella</span>
+                            </a>
+                            <a href="criptoplayvideo.php?id=9ivEr2XP-r8" class="grid_3">
+                                <img src="images/assets/nuevo.jpg" alt="#">
+                                <span><strong>Los Criptozoos </strong>Nada nuevo</span>
+                            </a>
+                            <a href="criptoplayvideo.php?id=uZdxKTz5CuA" class="grid_3">
+                                <img src="images/assets/grao.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>Yo podria</span>
+                            </a>
+                            <a href="criptoplayvideo.php?id=PFrckDdQIcg" class="grid_3">
+                                <img src="images/assets/vila.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>Rock de combate</span>
+                            </a>
+
+                        </div><!-- video grid -->
+                    </li><!-- tab content -->
+
+                    <li id="Featured">
+                        <div class="video-grid">
+                            <a href="criptoplayvideo.php?id=Xahew1vjZ3c" class="grid_3">
+                                <img src="images/assets/camps2.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>El clan de la muerte (video montaje)</span>
+                            </a>
+
+                            <a href="criptoplayvideo.php?id=tp6fYmIM6EY" class="grid_3">
+                                <img src="images/assets/joni.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>Johnny (de marjal making of)</span>
+                            </a>
+
+                        </div><!-- video grid -->
+                    </li><!-- tab content -->
+
+                    <li id="Lightbox">
+                        <div class="video-grid">
+                            <a href="criptoplayvideo.php?id=3eZuo9bKCFw" class="grid_3">
+                                <img src="images/assets/bcm.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>The BCFM Sunday Rock Sow</span>
+                            </a>
+                            <a href="criptoplayvideo.php?id=QKYN7u8HSRs" class="grid_3">
+                                <img src="images/assets/danza.jpg" alt="#">
+                                <span><strong>Los Criptozoos</strong>The BCFM Sunday Rock Sow</span>
+                            </a>
+
+                        </div><!-- video grid -->
+                    </li><!-- tab content -->
+                </ul><!-- end tabs -->
+            </div><!-- def block -->
+        </div><!-- posts -->
+
+    </div><!-- row clearfix -->
+</div><!-- end page content -->
+
+<footer id="footer">
+    <div class="footer-last">
+        <div class="row clearfix">
+            <span class="copyright">© 2014 <a href="http://loscriptozoos.com">Los Criptozoos</a>.</span>
+
+            <div id="toTop"><i class="icon-angle-up"></i></div><!-- Back to top -->
+            <div class="foot-menu">
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="cripto-musica.php">Música.MP3</a></li>
+                    <li><a href="cripto-videos.php">Videos</a></li>
+                    <li><a href="cripto-fotos.php">Galeria de fotos</a></li>
+                    <li><a href="cripto-eventos.php">Eventos</a></li>
+                    <li><a href="cripto-contacto.php">Contacto</a></li>
+                </ul><!-- end links -->
+            </div><!-- end foot menu -->
+        </div><!-- row -->
+    </div><!-- end last -->
+
+</footer><!-- end footer -->
+
+</div><!-- end layout -->
+<!-- Scripts -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/band.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/twitter/jquery.tweet.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="customize/script.js"></script><link rel="stylesheet" type="text/css" href="customize/style.css" media="screen" /><style id="themecolors" type="text/css"></style>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-50186218-1', 'loscriptozoos.com');
+    ga('send', 'pageview');
+
+</script>
+
+
+
+</body>
+</html>
---
+++ criptoplayvideo.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<!--[if IE 7 ]><html class="ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie8" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="ie9" xmlns="http://www.w3.org/1999/xhtml" lang="en-US"> <![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!-->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+<!--<![endif]-->
+<head>
+    <title>Videos | Los Criptozoos</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <!-- Seo Meta -->
+    <meta name="description" content="Video Single Wide | Band">
+    <meta name="keywords" content="video, remix, music, light, dark, multi purpose, band, css3, html5">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.min.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/style.css" id="dark" media="screen" />
+    <link rel="stylesheet" type="text/css" href="styles/icons/icons.css" media="screen" />
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+    <!--[if IE]>
+    <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=EmulateIE8; IE=EDGE" />
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <link rel="stylesheet" type="text/css" href="styles/icons/font-awesome-ie7.min.css" />
+    <![endif]-->
+</head>
+
+<!-- popup login -->
+
+<!-- popup login -->
+
+<header id="header" class="glue">
+    <div class="row clearfix">
+
+
+            <div class="social social-head">
+                <a href="http://twitter.com/@loscriptozoos" class="bottomtip" title="Síguenos en Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                <a href="http://facebook.com/loscriptozoos" class="bottomtip" title="Me gusta en Facebook" target="_blank"><i class="icon-facebook"></i></a>
+
+            </div><!-- end social -->
+
+            <!-- search -->
+        <!-- little head -->
+    </div><!-- row -->
+
+    <div class="headdown">
+        <div class="row clearfix">
+            <div class="logo bottomtip" title="Best and Most Popular Musics">
+                <a href="#"><img src="./crypto_imagenes/cripcrip.png" alt="Best and Most Popular Musics"></a>
+            </div><!-- end logo -->
+
+            <nav>
+                <ul class="sf-menu">
+                    <li>
+                        <a href="index.php">Inicio<span class="sub">Comienza aquí</span></a>
+                    </li>
+                    <li><a href="cripto-about.php">Sobre la banda<span class="sub">Los Criptozoos</span></a></li>
+                    <li class="current selectedLava">
+                    <a href="cripto-musica.php">Material<span class="sub">Contenido digital</span></a>
+                        <ul>
+                            <li><a href="cripto-fotos.php">Galeria de fotos</a></li>
+                            <li><a href="cripto-musica">Música</a></li>
+                            <li><a href="cripto-videos.php">Videos</a></li>
+                        </ul>
+
+                    </li>
+                    <li><a href="cripto-eventos.php">Eventos<span class="sub">Próximos eventos</span></a></li>
+                    <li>
+                        <a href="cripto-contacto.php">Contacto<span class="sub">Contacta con nosotros</span></a>
+                    </li>
+                </ul><!-- end menu -->
+            </nav><!-- end nav -->
+
+        </div><!-- row -->
+    </div><!-- headdown -->
+</header><!-- end header -->
+
+<div class="under_header">
+    <img src="images/assets/breadcrumbs14.png" alt="#">
+</div><!-- under header -->
+
+<div class="page-content back_to_up">
+    <div class="row clearfix mb">
+        <div class="breadcrumbIn">
+            <ul>
+                <li><a href="index.php" class="toptip" title="Homepage"> <i class="icon-home"></i> </a></li>
+                <li><a href="cripto-videos.php"> Videos </a></li>
+                <li> Criptovideos </li>
+            </ul>
+        </div><!-- breadcrumb -->
+    </div><!-- row -->
+
+    <div class="row row-fluid clearfix mbf">
+        <div class="posts">
+            <div class="def-block">
+                <div class="post row-fluid clearfix">
+                    <iframe  height="500" src="//www.youtube.com/embed/<?php echo $_GET['id']; ?>?rel=0" frameborder="0" allowfullscreen></iframe>
+
+    <!--            <h3 class="post-title"> <a href="http://loscriptozoos.com/criptoplayvideo.php?id=tp6fYmIM6EY">Grabado y editado por DeMarjal</a> </h3>
+                    <p>Making of de la grabación del tema "Johnny" de Los Criptozoos para el recopilatorio Black Ice 2014..</p>   -->
+
+                    <p>
+                        <span> Tags: </span>
+                        <a href="#" class="#"> video </a>,
+                        <a href="#" class="#"> player </a>,
+                        <a href="#" class="#"> youtube </a>,
+      <!--              <a href="#" class="#"> vimeo </a> -->
+                    </p><!-- tags -->
+
+                    <div class="meta">
+                        <span> <i class="icon-user mi"></i> elchupacabras </span>
+             <!--       <span> <i class="icon-time mi"></i>August 20, 2013 2:00 AM </span>  -->
+                    </div><!-- meta -->
+
+
+                    <!-- like -->
+                </div><!-- post -->
+
+                <!-- post links -->
+
+                <!-- Disqus Comment Form -->
+
+                <!-- Disqus Comment Form -->
+
+            </div><!-- def block -->
+        </div><!-- span8 posts -->
+
+    </div><!-- row clearfix -->
+</div><!-- end page content -->
+
+<footer id="footer">
+    <div class="footer-last">
+        <div class="row clearfix">
+            <span class="copyright">© 2014 by <a href="#">loscriptozoos.com</a>. All Rights Reserved. Powered by <a href="#">Loscriptozoos.com</a>.</span>
+
+            <div id="toTop"><i class="icon-angle-up"></i></div><!-- Back to top -->
+            <div class="foot-menu">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="mp3s.html">MP3</a></li>
+                    <li><a href="videos.html">Video</a></li>
+                    <li><a href="gallery.html">Photo Gallery</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul><!-- end links -->
+            </div><!-- end foot menu -->
+        </div><!-- row -->
+    </div><!-- end last -->
+
+</footer><!-- end footer -->
+
+<!-- end layout -->
+<!-- Scripts <script id="disqus-forumData" type="text/json"><script id="disqus-forumData" type="text/json">-->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/band.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+<script type="text/javascript" src="js/jquery.flexslider-min.js"></script>
+<script type="text/javascript" src="js/jquery.nanoscroller.js"></script>
+<script type="text/javascript" src="js/twitter/jquery.tweet.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+    /* <![CDATA[ */
+    // Disqus
+    var disqus_shortname = 'remixtemplate';
+    (function () {
+        var s = document.createElement('script'); s.async = true;
+        s.type = 'text/javascript';
+        s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+    }());
+    /* ]]> */
+</script>
+<script type="text/javascript" src="customize/script.js"></script><link rel="stylesheet" type="text/css" href="customize/style.css" media="screen" /><style id="themecolors" type="text/css"></style>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-50186218-1', 'loscriptozoos.com');
+    ga('send', 'pageview');
+
+</script>
+
+
+</body>
+</html>
